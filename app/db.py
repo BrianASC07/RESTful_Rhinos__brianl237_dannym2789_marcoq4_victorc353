ae70111289c68c260fa2239153dac6a5322d540d
Danny <dmok50@stuy.edu>
2024-12-06 11:01:49 -0500

    updated db

---
+++ app/db.py
@@ -0,0 +1,45 @@
+import sqlite3, csv
+
+# Create Tables
+def createTables():
+    db = sqlite3.connect("RESTables.db")#, check_same_thread = False)
+    c = db.cursor()
+    
+    #User Info
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS users {
+                id INTEGER PRIMARY KEY,
+                username TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL
+            }
+        ''')
+    
+    #Stocks Info
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS stocks {
+                id INTEGER PRIMARY KEY,
+            }
+        ''') # will need to add all stocks later with API calls
+    
+    #Weather Regions Info
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS weather {
+                id INTEGER PRIMARY KEY,
+            }
+        ''') # will need to add all stocks later with API calls
+    
+    #News Preferences Info
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS news {
+                id INTEGER PRIMARY KEY,
+            }
+        ''') # will need to add all stocks later with API calls
+    
+    #Holiday Regions Info
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS holidays {
+                id INTEGER PRIMARY KEY,
+            }
+        ''') # will need to add all stocks later with API calls
+    
+    
