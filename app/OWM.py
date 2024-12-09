6d52bf5199a02e00acdf67445ed8183f366c4b90
Marco Quintero <mquintero50@stuy.edu>
2024-12-09 10:34:03 -0500

    Finished testing of OWM API. Will transfer findings to __init__.py

--- app/OWM.py
+++ app/OWM.py
@@ -16,12 +16,12 @@ def main():
     latURL = coords[0]["lat"]
     lonURL = coords[0]["lon"]
     print(f"https://api.openweathermap.org/data/2.5/weather?lat={latURL}&lon={lonURL}&appid=" + api_key)
-    with urllib.request.urlopen(f"https://api.openweathermap.org/data/2.5/weather?{latURL}=&{lonURL}=&appid=" + api_key) as response:
+    with urllib.request.urlopen(f"https://api.openweathermap.org/data/2.5/weather?lat={latURL}&lon={lonURL}&appid=" + api_key) as response:
         data = json.loads(response.read()) #reads the page's source code and converts to python dictionary in the same line
         #print(data)
     #print(coords["lat"])
     #print(coords[0])
-    return render_template('OWM_test.html', lat=coords[0]["lat"], lon=coords[0]["lon"], country=coords[0]["country"], state = coords[0]["state"])
+    return render_template('OWM_test.html', lat=coords[0]["lat"], lon=coords[0]["lon"], country=coords[0]["country"], state = coords[0]["state"], temp = data["main"]["temp"], main = data["weather"][0]["main"], description = data["weather"][0]["description"])
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
