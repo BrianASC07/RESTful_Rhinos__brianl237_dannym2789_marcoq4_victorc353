9a38b0ac8e86f4cb3bf01fa7d7376d63aec786c8
Brian Liu <109633886+BrianASC07@users.noreply.github.com>
2024-12-10 08:12:32 -0500

    readded key checks

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,40 @@ from flask import Flask, render_template, url_for, session, request, redirect
 import os, json, urllib.request
 
 from APIModule import Calendarific, OWM, FMP
+#KEY TESTING
+keys_missing = False
 
+FMP_key = ""
+Open_Weather_Map_key = ""
+NYT_key = ""
+Calendarific_key = ""
+try:
+    FMP = open("../keys/key_FMP.txt", "r")
+    FMP_key = FMP.read()
+    print("FMP KEY LOADED")
+
+    NYT = open("../keys/key_NYT.txt", "r")
+    NYT_key = NYT.read()
+    print("NYT KEY LOADED")
+
+    Calendarific = open("../keys/key_Calendarific.txt", "r")
+    Calendarific_key = Calendarific.read()
+    print("CALENDARIFIC KEY LOADED")
+
+    Open_Weather_Map = open("../keys/key_Open_Weather_Map.txt", "r")
+    Open_Weather_Map_key = Open_Weather_Map.read()
+    print("OPEN WEATHER MAP LOADED \n")
+except:
+    print("API KEY FILES MISSING")
+    keys_missing = True
+
+if(FMP_key == "" or Open_Weather_Map_key == "" or NYT_key == "" or Calendarific_key == ""):
+    print("API KEYS MISSING:")
+    print("FMP: " + FMP_key)
+    print("OWM: " + Open_Weather_Map_key)
+    print("NYT: " + NYT_key)
+    print("CAL: " + Calendarific_key)
+    keys_missing = True
 
 app = Flask(__name__)
 
