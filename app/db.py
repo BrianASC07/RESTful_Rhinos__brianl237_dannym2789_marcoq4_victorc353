5a9737b23941b20f29d02d8ee68d8bc460a0bcd6
Danny <dmok50@stuy.edu>
2024-12-10 11:02:03 -0500

    db issues

--- app/db.py
+++ app/db.py
@@ -7,27 +7,27 @@ def createTables():
     
     #User Info
     c.execute('''
-            CREATE TABLE IF NOT EXISTS users {
+            CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY,
                 username TEXT UNIQUE NOT NULL,
                 password TEXT NOT NULL
-            }
+            )
         ''')
     
     #Stocks Info
-    c.execute('CREATE TABLE IF NOT EXISTS stocks {id INTEGER PRIMARY KEY,}')
+    c.execute('CREATE TABLE IF NOT EXISTS stocks (id INTEGER PRIMARY KEY)')
     # will need to add all stocks later with API calls
     
     #Weather Regions Info
-    c.execute('CREATE TABLE IF NOT EXISTS weather {id INTEGER PRIMARY KEY,}')
+    c.execute('CREATE TABLE IF NOT EXISTS weather (id INTEGER PRIMARY KEY)')
     # will need to add all weather later with API calls
     
     #News Preferences Info
-    c.execute('CREATE TABLE IF NOT EXISTS news {id INTEGER PRIMARY KEY,}')
+    c.execute('CREATE TABLE IF NOT EXISTS news (id INTEGER PRIMARY KEY)')
     # will need to add all news later with API calls
     
     #Holiday Regions Info
-    c.execute('CREATE TABLE IF NOT EXISTS holidays {id INTEGER PRIMARY KEY,}')
+    c.execute('CREATE TABLE IF NOT EXISTS holidays (id INTEGER PRIMARY KEY)')
     # will need to add all holidays later with API calls
     
     db.commit()
@@ -36,17 +36,10 @@ def createTables():
 def addColumn(table, colname):
     db = sqlite3.connect("RESTables.db")#, check_same_thread = False)
     c = db.cursor()
-    c.execute('ALTER TABLE ? ADD ? TEXT', (table, column))
+    c.execute('ALTER TABLE ' + table + ' ADD ' + colname + ' TEXT')
     db.commit()
     db.close()
 
-'''
-def fxnTemplate():
-    db = sqlite3.connect("RESTables.db")#, check_same_thread = False)
-    c = db.cursor()
-    c.execute('''
-        
-        ''')
-    db.commit()
-    db.close()
-'''
+createTables()
+addColumn("stocks", "ooga")
+
