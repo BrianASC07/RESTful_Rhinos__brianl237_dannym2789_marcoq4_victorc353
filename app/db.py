8e05473bc36a997516e79f3d22f00e2df7bc6b75
Danny <dmok50@stuy.edu>
2024-12-16 10:56:16 -0500

    DB update for NYT

--- app/db.py
+++ app/db.py
@@ -1,9 +1,77 @@
 import sqlite3, csv, APIModule.FMP
 
 def getNewsSections():
-    return ["arts", "automobiles", "books", "business", "fashion", "food", "health", "home", "insider", "magazine", "movies", "nyregion",
-        "obituaries", "opinion", "politics", "realestate", "science", "sports", "sundayreview", "technology", "theater", "travel", "upshot"
-    "us", "world"] #to minimize api calls
+    return [
+    "Arts",
+    "Automobiles",
+    "Autos",
+    "Blogs",
+    "Books",
+    "Booming",
+    "Business",
+    "Business Day",
+    "Corrections",
+    "Crosswords & Games",
+    "Crosswords/Games",
+    "Dining & Wine",
+    "Dining and Wine",
+    "Editors' Notes",
+    "Education",
+    "Fashion & Style",
+    "Food",
+    "Front Page",
+    "Giving",
+    "Global Home",
+    "Great Homes & Destinations",
+    "Great Homes and Destinations",
+    "Health",
+    "Home & Garden",
+    "Home and Garden",
+    "International Home",
+    "Job Market",
+    "Learning",
+    "Magazine",
+    "Movies",
+    "Multimedia",
+    "Multimedia/Photos",
+    "N.Y. / Region",
+    "N.Y./Region",
+    "NYRegion",
+    "NYT Now",
+    "National",
+    "New York",
+    "New York and Region",
+    "Obituaries",
+    "Olympics",
+    "Open",
+    "Opinion",
+    "Paid Death Notices",
+    "Public Editor",
+    "Real Estate",
+    "Science",
+    "Sports",
+    "Style",
+    "Sunday Magazine",
+    "Sunday Review",
+    "T Magazine",
+    "T:Style",
+    "Technology",
+    "The Public Editor",
+    "The Upshot",
+    "Theater",
+    "Times Topics",
+    "TimesMachine",
+    "Today's Headlines",
+    "Topics",
+    "Travel",
+    "U.S.",
+    "Universal",
+    "UrbanEye",
+    "Washington",
+    "Week in Review",
+    "World",
+    "Your Money"
+] #to minimize api calls
 def getStockList():
     return [
 'AEP',
@@ -286,7 +354,7 @@ def createTables():
 
     executable = "CREATE TABLE IF NOT EXISTS newsContentPreferences (userID INTEGER, "
     for i in sections:
-        executable = executable + i + " INTEGER, " #0 if user doesnt care, 1 if user does (each user is a row)
+        executable = executable + '"' + i + '"' + " INTEGER, " #0 if user doesnt care, 1 if user does (each user is a row)
 
     executable = executable[:-2] + ")"
 
