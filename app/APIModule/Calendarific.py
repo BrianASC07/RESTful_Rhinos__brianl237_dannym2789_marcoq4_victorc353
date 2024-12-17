1aa7651f0ca7b78399d7b53a47b14f9ffe668253
Brian Liu <109633886+BrianASC07@users.noreply.github.com>
2024-12-16 20:33:08 -0500

    Update Calendarific.py

--- app/APIModule/Calendarific.py
+++ app/APIModule/Calendarific.py
@@ -9,9 +9,9 @@ def getHoliday(state):
         file = open("../keys/key_Calendarific.txt", "r")
         api_key = file.read().strip()
         if state == "":
-            url=f"https://calendarific.com/api/v2/holidays?&api_key={api_key}&country=us&year={current_time.year}&month={current_time.month}&day={current_time.day + 2}"
+            url=f"https://calendarific.com/api/v2/holidays?&api_key={api_key}&country=us&year={current_time.year}&month={current_time.month}&day={current_time.day}"
         else: 
-            url=f"https://calendarific.com/api/v2/holidays?&api_key={api_key}&country=us&year={current_time.year}&location=us-{state}&month={current_time.month}&day={current_time.day + 2}"
+            url=f"https://calendarific.com/api/v2/holidays?&api_key={api_key}&country=us&year={current_time.year}&location=us-{state}&month={current_time.month}&day={current_time.day}"
 
         #weird security thing in calendarific only authorizes requests from a browser for some reason i think??? workaround below
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} #from https://stackoverflow.com/questions/65389552/python-requests-not-working-for-a-website-api-but-works-in-browser-chrome
@@ -37,9 +37,9 @@ def getHolidayInfo(state):
         file = open("../keys/key_Calendarific.txt", "r")
         api_key = file.read().strip()
         if state == "":
-            url=f"https://calendarific.com/api/v2/holidays?&api_key={api_key}&country=us&year={current_time.year}&month={current_time.month}&day={current_time.day + 2}"
+            url=f"https://calendarific.com/api/v2/holidays?&api_key={api_key}&country=us&year={current_time.year}&month={current_time.month}&day={current_time.day}"
         else: 
-            url=f"https://calendarific.com/api/v2/holidays?&api_key={api_key}&country=us&year={current_time.year}&location=us-{state}&month={current_time.month}&day={current_time.day + 2}"
+            url=f"https://calendarific.com/api/v2/holidays?&api_key={api_key}&country=us&year={current_time.year}&location=us-{state}&month={current_time.month}&day={current_time.day}"
 
         #weird security thing in calendarific only authorizes requests from a browser for some reason i think??? workaround below
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} #from https://stackoverflow.com/questions/65389552/python-requests-not-working-for-a-website-api-but-works-in-browser-chrome
